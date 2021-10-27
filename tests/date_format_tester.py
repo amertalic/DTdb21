95c24b8561d1a106eec95ffaba8152775e79c69e
Amer Talic <amertalic@gmail.com>
2021-10-27 22:39:23 +0200

    updated possible date formats for testing

--- tests/date_format_tester.py
+++ tests/date_format_tester.py
@@ -2,7 +2,7 @@ import datetime
 from utils import date_cleaner
 
 # list of possible and seen date formats extracted from excel
-date_list = [datetime.datetime(2021, 1, 15), datetime.date(2021, 2, 15), '2021-3-23', '2021-14-01', '22.5.2010',]
+date_list = [datetime.datetime(2021, 1, 15), datetime.date(2021, 2, 15), '2021-3-23', '2021-14-01', '22.5.2010', '2033/10/01', '21-Feb-03','21-April-03','2021-April-03']
 
 # test all possible date formats
 for date in date_list:
