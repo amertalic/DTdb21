f7e561b450c9fb2c3c048c512f5f15c621c4c810
Amer Talic <amertalic@gmail.com>
2021-11-15 08:18:07 +0100

    loop through excel files - not finished

--- data/file_processing.py
+++ data/file_processing.py
@@ -1,6 +1,6 @@
 import os
 
-directory = os.fsencode('excel_unprocessed')
+directory = os.fsencode('C:/Users/AT/Documents/git_projects/DTdb21/data/excel_files_folder/excel_unprocessed')
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
