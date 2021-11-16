56add995b8c08d02bea7bf520769061592c57cef
Amer Talic <amertalic@gmail.com>
2021-11-16 07:13:20 +0100

    loop through excel files - update

--- data/file_processing.py
+++ data/file_processing.py
@@ -1,7 +1,12 @@
 import os
 
-directory = os.fsencode('C:/Users/AT/Documents/git_projects/DTdb21/data/excel_files_folder/excel_unprocessed')
+directory = os.fsencode("excel_files_folder/excel_unprocessed")
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    print(os.path.join(directory, filename))
+    if filename in ['__init__.py', '__pycache__']:
+        continue
+    else:
+        print(os.path.join("excel_files_folder/excel_unprocessed", filename))
+
+
