d6361c2d21d2848469f9d42f06eb0fb9f912bab5
Amer Talic <amertalic@gmail.com>
2021-11-18 07:26:07 +0100

    experimenting with pandas

--- data/file_processing.py
+++ data/file_processing.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 directory = os.fsencode("excel_files_folder/excel_unprocessed")
 
@@ -8,3 +9,10 @@ for file in os.listdir(directory):
         continue
     else:
         print(os.path.join(os.getcwd(),"excel_files_folder/excel_unprocessed", filename))
+        excel = pd.read_excel(os.path.join(os.getcwd(), "excel_files_folder/excel_unprocessed", filename))
+
+        print(excel)
+
+
+
+
